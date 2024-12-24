1f414aa85cdc6cb2370f637437ab3d043db2153d
joaovjm <joao.oliveira18.jm@gmail.com>
2024-12-24 14:37:32 -0300

    Estrutura base inicial do Lekobox

---
+++ src/pages/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import '../styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Esse será o App Lekobox. Sistema simples para genrenciar chaves de ativação e clientes</h1>
+    </>
+  )
+}
+
+export default App
