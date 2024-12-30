68f22c6a70ff731d8440b05d0a7b8be10467b8fb
joaovjm <joao.oliveira18.jm@gmail.com>
2024-12-30 01:26:10 -0300

    App.jsx modificado para receber as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css'
-import Container from './assets/components/Container'
-import Footer from './assets/components/Footer'
-import Header from './assets/components/Header'
+import AppRoutes from './routes'
 
 function App() {
 
   return (
     <>
-       <Header/>
-       <Container/>
-       <Footer/>
+       <AppRoutes/>
     </>
   )
 }
